e17b57903e9ebdfa14f0de13430824b57e9493be
shem <shemaduida@gmail.com>
2024-08-06 15:53:28 +0300

    🎨 configured firebase

--- apps/angular-monorepo/src/main.ts
+++ apps/angular-monorepo/src/main.ts
@@ -1,7 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../../../libs/elements/src/lib/environments/environment';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      AngularFirestoreModule
+    )
+  ]
+}).catch((err) => console.error(err));
